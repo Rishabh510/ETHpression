5b8c6d0d11ff81013d9ceb85b503fadc9f778dcf
Rishabh Raizada <rishabh5102000@gmail.com>
2022-04-14 22:34:24 +0530

    Added lazy minting

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,68 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import Moralis from "moralis/dist/moralis.min.js";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const appId = "kXjoOhQniSv6NAkrlceELZFoafLjL5C1xANVV30w";
+  const serverUrl = "https://deoqbw6qywmq.usemoralis.com:2053/server";
+  const [user, setUser] = useState();
+  const [nft, setNft] = useState("");
+
+  const moralisInit = async () => {
+    Moralis.start({ serverUrl, appId });
+    setUser(Moralis.User.current());
+    if (!user) {
+      try {
+        let curruser = await Moralis.authenticate({
+          signingMessage: "Hello World!",
+        });
+        setUser(curruser);
+        // lazyMint();
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      Moralis.enableWeb3();
+      // lazyMint();
+    }
+  };
+
+  const lazyMint = async () => {
+    console.log(Moralis.Plugins);
+    const res = await Moralis.Plugins.rarible.lazyMint({
+      chain: "rinkeby",
+      userAddress: "0xc76235d153e17e410BB4B9D3bF5312F9758cf274",
+      tokenType: "ERC721",
+      tokenUri: "/ipfs/QmWLsBu6nS4ovaHbGAXprD1qEssJu4r5taQfB74sCG51tp",
+      supply: 100,
+      royaltiesAmount: 5, // 0.05% royalty. Optional
+    });
+    console.log(res);
+    setNft(
+      `https://rinkeby.rarible.com/token/${res.data.result.tokenAddress
+        .toString()
+        .toLowerCase()}:${res.data.result.tokenId}`
+    );
+  };
+
+  useEffect(() => {
+    moralisInit();
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+          <button type="button" onClick={() => lazyMint()}>
+            count
           </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
+          <a href={nft}>
+            <button>View</button>
           </a>
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
